042d598355a75b66abc100d24ea4a0dbc913f6ad
emaema1128 <y.on.dw3@gmail.com>
2024-01-06 23:26:01 +0900

    出品画面推移とactive_hashの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,14 +1,27 @@
 class ItemsController < ApplicationController
-  # before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, except: [:index, :show]
 
-  # def index
-  # end
+  def index
+    # @items = Item.all
+  end
 
-  # def new
-  #   @items = Item.new
-  # end
+  def new
+    # @item = Item.new
+  end
 
-  # def create
-  # end
+  def create
+    @items = Item.create(item_params)
+    if @items.save
+      redirect_to '/'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 
+
+
+  private
+  def item_params
+    params.require(:item).permit(:image, :text)
+  end
 end
