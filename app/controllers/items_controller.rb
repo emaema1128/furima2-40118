765d5c62da3b0450ca6b00f181d7a2d2e7637675
emaema1128 <y.on.dw3@gmail.com>
2024-01-09 20:41:38 +0900

    他ユーザーの商品編集へのアクセスができないようにする

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,6 +24,18 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
+    redirect_to root_path unless current_user.id == @item.user_id
+
+  end
+
+  def update
+    @item = Item.find(params[:id])
+
+    if @item.update(item_params)
+      redirect_to item_path(params[:id])
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   private
