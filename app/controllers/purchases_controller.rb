d5a2877b675dd23d93217234ab8d27bc22ca94c9
emaema1128 <y.on.dw3@gmail.com>
2024-01-10 16:53:27 +0900

    コントローラー定義・エラーメッセージを表示するための部分テンプレートを作成

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -2,9 +2,24 @@ class PurchasesController < ApplicationController
 
   def index
     @item = Item.find(params[:item_id])
+    @purchase = Purchase.new
   end
 
-  def new
+  def create
+    @purchase = Purchase.new(purchase_params)
+    if @purchase.valid?
+      @purchase.save
+      return redirect_to root_path
+    else
+      render 'index', status: :unprocessable_entity
+    end
   end
+
+  private
+
+  def purchase_params
+    params.require(:purchase).permit(:price)
+  end
+
 
 end
--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,12 @@
 class Address < ApplicationRecord
+
+  validates :postalcode, presence: true
+  validates :prefecture_id, presence: true
+  validates :city, presence: true
+  validates :block, presence: true
+  validates :building, presence: true
+  validates :phone_number, presence: true
+  validates :price, presence: true
+
+  block
 end
