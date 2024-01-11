78896bc996c398e529ade63fbf5e69bc119abcff
emaema1128 <y.on.dw3@gmail.com>
2024-01-12 02:48:26 +0900

    単体テストコード

---
+++ spec/factories/purchase_addresses.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :purchase_address do
+    token { "tok_abcdefghijk00000000000000000" }
+    postalcode { '123-4567' }
+    prefecture_id { "3" }
+    city_id { '横浜市緑区' }
+    block { '青山1-1' }
+    building { '東京ハイツ' }
+    phone_number { '09012345678' }
+  end
+end
